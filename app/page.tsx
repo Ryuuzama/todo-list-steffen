ac0eed6c9f35421c572687f2582f43a022266163
Ryuuzama <danjstef@gmail.com>
2024-12-15 15:42:37 -0700

    comment to force reload

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from "react";
 import { fetchTasks, addTask, updateTask, deleteTask } from "../lib/api/tasks";
 
+//task type
+
 interface Task {
   id: string;
   title: string;
