20514a1f903b46b0dd51a674c4a821346c30bcd4
Ryuuzama <danjstef@gmail.com>
2024-12-14 19:06:55 -0700

    made update work again

--- README.md
+++ README.md
@@ -12,19 +12,28 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
 ## Deploy on Vercel
 
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
+First connect your project to git.
+
+```bash
+git init
+```
+
+then go to github.com and create a new repository on your account. You can then copy the repository link and connect it to your project by making your repository the origin:
+
+```bash
+git remote add origin <your-repository-link>
+```
+
+Once it is linked, commit and push your changes to your repository. (You can do this on the left-hand side of vscode in the source control tab).
+
+Once it has been updated on github, go back to vercel and find the giant box that says "Import Git Repository" and click import on your repository name. If you don't see your repository name, you might have to change app permissions. There is a button under the import tab that says "adjust GitHub App Permissions. You can manually select your repository to have permission to be used by vercel there.
+
+Once you click import, you can literally just click "deploy" and it will build your application for you and give you a free domain that is publically accessbile.
+
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,8 @@ interface Task {
     created: Date;
   }
 
-//CREATE function
+//CREATE function-------------
+
 function AddTask({ tasks, setTasks }: { tasks: Task[]; setTasks: React.Dispatch<React.SetStateAction<Task[]>> }){ //React.Dispatch is used in case we ever pass in just an array or a function that returns a new array.
 
   const [userInputTaskName, setUserInputTaskName] = useState<string>('');
@@ -65,7 +66,8 @@ function AddTask({ tasks, setTasks }: { tasks: Task[]; setTasks: React.Dispatch<
 
 }
 
-//DELETE function
+//DELETE function---------------
+
 function handleDelete({ tasks, setTasks, id }: { tasks: Task[]; setTasks: React.Dispatch<React.SetStateAction<Task[]>>; id: number }){
     const updatedTasks = tasks.filter((task) => task.id !== id);
     setTasks(updatedTasks);
@@ -73,15 +75,17 @@ function handleDelete({ tasks, setTasks, id }: { tasks: Task[]; setTasks: React.
 
 const App = () => {
   const [tasks, setTasks] = useState<Task[]>([]); //the ([]) here is the initial state which is an empty array
-  const [editingTaskId, setEditingTaskId] = useState<number | null>(null);
+  const [editingNameId, setEditingNameId] = useState<number | null>(null);
+const [editingDescriptionId, setEditingDescriptionId] = useState<number | null>(null);
   const [editedUserInputTaskName, setUserInputEditedTaskName] = useState<string>("");
   const [editedUserInputTaskDescription, setUserInputEditedTaskDescription] = useState<string>("");
   const [filterStatus, setFilterStatus] = useState<"all" | "completed" | "pending">("all"); //used for filtering tasks. the "("all")" at the end of the line is the initialization
 
-  //UPDATE functions
+  //UPDATE constants-------------
+
   const handleNameClick = (task: Task) => {
     //selects the task id that you clicked
-    setEditingTaskId(task.id);
+    setEditingNameId(task.id);
     setUserInputEditedTaskName(task.title);
   }
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -93,12 +97,12 @@ const App = () => {
     //uses setTasks to actually make a new array of tasks as the current array, while also changing the title if editedTaskId is the same as one of the taskId's it is looping through with .map
     setTasks((prevTasks) => prevTasks.map((task) => task.id === taskId ? {...task, title: editedUserInputTaskName }: task)); //prevTasks is just a name to represent the old task array (react requires this array when doing setTasks). Then we loop through it with .map, and for each "task" we check if the id matches the taskId we passed in (which is the task we are editing. then we change the title to the new title.) "...task" is the new task we will update.
 
-    setEditingTaskId(null);
+    setEditingNameId(null);
   }
 
   const handleDescriptionClick = (task: Task) => {
     //selects the task id that you clicked
-    setEditingTaskId(task.id);
+    setEditingDescriptionId(task.id);
     setUserInputEditedTaskDescription(task.description);
   }
   const handleDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -110,19 +114,22 @@ const App = () => {
     //uses setTasks to actually make a new array of tasks as the current array, while also changing the title if editedTaskId is the same as one of the taskId's it is looping through with .map
     setTasks((prevTasks) => prevTasks.map((task) => task.id === taskId ? {...task, description: editedUserInputTaskDescription }: task)); //prevTasks is just a name to represent the old task array (react requires this array when doing setTasks). Then we loop through it with .map, and for each "task" we check if the id matches the taskId we passed in (which is the task we are editing. then we change the title to the new title.) "...task" is the new task we will update.
 
-    setEditingTaskId(null);
+    setEditingDescriptionId(null);
   }
 
   const handleCompleted = (taskId: number) => {
     setTasks((prevTasks) => prevTasks.map((task) => task.id === taskId ? {...task, completed: !task.completed }: task));
   }
 
-  //FILTER functions
+  //FILTER functions----------------
+
   const filteredTasks = tasks.filter(task => {
     if (filterStatus === "all") return true;
     if (filterStatus === "completed") return task.completed;
     if (filterStatus === "pending") return !task.completed;
   });
+
+  //Rendering----------------
 
   return (
     <div>
@@ -157,10 +164,11 @@ const App = () => {
             key={task.id}
             style={ {textDecoration: task.completed ? "line-through": "none"}}>
             <td>
-              {editingTaskId === task.id ? (
+              {editingNameId === task.id ? (
                 <input 
                   type="text"
                   value={editedUserInputTaskName}
+                  className="bg-primary"
                   onChange={(event) => handleNameChange(event)} 
                   onBlur={() => handleNameSubmit(task.id)}
                   onKeyDown={(event) => {
@@ -177,10 +185,11 @@ const App = () => {
               }
             </td>
             <td>
-            {editingTaskId === task.id ? (
+            {editingDescriptionId === task.id ? (
                 <input 
                   type="text"
                   value={editedUserInputTaskDescription}
+                  className="bg-primary"
                   onChange={(event) => handleDescriptionChange(event)} 
                   onBlur={() => handleDescriptionSubmit(task.id)}
                   onKeyDown={(event) => {
