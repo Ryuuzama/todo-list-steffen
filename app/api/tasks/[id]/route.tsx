2fb6d1c192ca91bab20f86238566526fef8aa9c1
Ryuuzama <danjstef@gmail.com>
2024-12-15 03:15:52 -0700

    another change to parameter deployement issue

--- app/api/tasks/[id]/route.tsx
+++ app/api/tasks/[id]/route.tsx
@@ -4,10 +4,10 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // Handle fetching, updating, and deleting a single task by ID
-export async function GET(req: Request, { fetchedID }: { fetchedID: { id: string } }) {
+export async function GET(req: Request, context: { params: { id: string }}) {
   try {
     const task = await prisma.task.findUnique({
-      where: { id: fetchedID.id },
+      where: { id: context.params.id },
     });
     if (!task) {
       return NextResponse.json({ error: "Task not found" }, { status: 404 });
@@ -19,11 +19,11 @@ export async function GET(req: Request, { fetchedID }: { fetchedID: { id: string
   }
 }
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, context: { params: { id: string } }) {
   try {
     const { title, description, completed } = await req.json();
     const updatedTask = await prisma.task.update({
-      where: { id: params.id },
+      where: { id: context.params.id },
       data: { title, description, completed },
     });
     return NextResponse.json(updatedTask);
@@ -33,9 +33,9 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
   }
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, context: { params: { id: string } }) {
     try {
-      const { id } = params;
+      const { id } = context.params;
   
       if (!id) {
         return NextResponse.json({ error: "Task ID is required" }, { status: 400 });
